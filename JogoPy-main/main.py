d673f6153ad5b64d403d1908c93fc5e7065ee0af
Maria EM <mariaeduardamueller0@gmail.com>
2024-07-12 18:42:42 -0300

    Add files via upload

---
+++ JogoPy-main/main.py
@@ -0,0 +1,63 @@
+from os.path import join
+from sys import exit
+
+import pygame
+import time
+from pytmx.util_pygame import load_pygame
+
+from config import *
+from nivel import Nivel
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        # Criação da janela de jogo
+        self.surface = pygame.display.set_mode((largura, altura), pygame.SCALED | pygame.HIDDEN)
+
+        janela = pygame.Window.from_display_module()
+        janela.size = (largura_janela, altura_janela)
+        janela.position = pygame.WINDOWPOS_CENTERED
+        janela.show()
+
+        pygame.display.set_caption(titulo)  # Título do jogo
+        self.clock = pygame.time.Clock()
+
+        self.num_nivel = 0
+        # Carregar mapas
+        self.tmx_maps = {
+            0: load_pygame(join("TiledLava.tmx")),
+            1: load_pygame(join("tiled\\TiledForest.tmx")),
+            2: load_pygame(join("tiled\\TiledSnow.tmx"))
+
+        }
+        self.current_stage = Nivel(self.tmx_maps[self.num_nivel], self.switch_stage)
+
+        self.tempo = time.time()
+
+    def switch_stage(self, num_nivel):
+        #  print(num_nivel)
+        self.current_stage = Nivel(self.tmx_maps[num_nivel], self.switch_stage)
+
+    def run(self):
+        while True:
+            self.clock.tick(FPS)
+            dt = time.time() - self.tempo
+            self.tempo = time.time()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            # Executar lógica do nível
+            self.current_stage.run(dt)
+
+            # Atualizar a tela
+        #    print(f"fps: {pygame.time.Clock.get_fps(self.clock):.0f}")
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    jogo = Game()
+    jogo.run()
---
+++ JogoPy-main/nivel.py
@@ -0,0 +1,134 @@
+import pygame.sprite
+
+from config import *
+from sprites import Sprite, Plataforma_Movel, Espinho
+from jogador import Jogador
+from groups import AllSprites
+from os.path import join
+
+
+class Nivel:
+    def __init__(self, tmx_map, switch_stage):
+        # Remover self.display_surface, já que vamos usar a game_surface da classe Game
+        self.num_niveis = None
+        self.level_finish_rect = None
+        self.pressed_botao = None
+        self.botao = None
+        self.jogador2 = None
+        self.jogador = None
+        self.switch_stage = switch_stage
+        self.display_surface = pygame.display.get_surface()
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+        self.semi_collision_sprites = pygame.sprite.Group()
+        self.dano_sprites = pygame.sprite.Group()
+        self.setup(tmx_map)
+
+    def setup(self, tmx_map):
+
+        # chao
+        for x, y, surf in tmx_map.get_layer_by_name("Chao").tiles():
+            Sprite((x * tamanho_bloco, y * tamanho_bloco), surf, (self.all_sprites, self.collision_sprites))
+        for x, y, surf in tmx_map.get_layer_by_name("Decoracao").tiles():
+            Sprite((x * tamanho_bloco, y * tamanho_bloco), surf, (self.all_sprites, ))
+
+        # objetos
+        for obj in tmx_map.get_layer_by_name("Player"):
+            if obj.name == "Passaro":
+                self.jogador = Jogador(pygame.image.load(join("tiled\\png\\chickenc.png")), (obj.x, obj.y),
+                                       (self.all_sprites,),
+                                       self.collision_sprites, self.semi_collision_sprites,
+                                       pygame.K_d, pygame.K_a, pygame.K_s, pygame.K_w)
+
+        for obj in tmx_map.get_layer_by_name("Player"):
+            if obj.name == "Player2":
+                self.jogador2 = Jogador(pygame.image.load(join("tiled\\png\\penguin.png")), (obj.x, obj.y),
+                                        (self.all_sprites,),
+                                        self.collision_sprites, self.semi_collision_sprites,
+                                        pygame.K_RIGHT, pygame.K_LEFT, pygame.K_DOWN, pygame.K_UP)
+
+        self.jogador.outro_jogador = self.jogador2
+        self.jogador2.outro_jogador = self.jogador
+        for obj in tmx_map.get_layer_by_name("Bandeira"):
+            if obj.name == "bandeira":
+                self.num_niveis = obj.properties["nivel"]
+                self.level_finish_rect = pygame.FRect((obj.x, obj.y), (obj.width, obj.height))
+        if tmx_map.get_layer_by_name("Obstaculo"):
+
+            for obj in tmx_map.get_layer_by_name("Obstaculo"):
+
+                if obj.name == "botao":
+                    self.pressed_botao = obj.properties["pressed_botao"]
+                    self.botao = pygame.FRect((obj.x, obj.y), (obj.width, obj.height))
+        for obj in tmx_map.get_layer_by_name("Dano"):
+                groups = self.all_sprites
+
+                if obj.name == "espinho":
+                    direcao_movi = obj.properties["direcao"]
+                    vel = 30
+                    if direcao_movi == "y":
+                            pos_inicial = (obj.x, obj.y)
+                            pos_final = (obj.x , obj.y + 100)
+                            self.espinho = Espinho((self.all_sprites, self.dano_sprites),
+                                                                 pos_inicial, pos_final, direcao_movi, vel, obj.width,obj.height)
+
+                    else:
+                            pos_inicial = (obj.x, obj.y)
+                            pos_final = (obj.x + 50, obj.y)
+                            self.espinho = Espinho((self.all_sprites, self.dano_sprites),
+                                                                pos_inicial,  pos_final,direcao_movi,vel,  obj.width, obj.height)
+
+
+        # Plataformas
+        for obj in tmx_map.get_layer_by_name("Plataformas"):
+            if obj.name == "Local":
+                # horizontal
+                if obj.width > obj.height:
+                    direcao_movi = "x"
+                    pos_inicial = (obj.x, obj.y + obj.height / 2)
+                    pos_final = (obj.x + obj.width, obj.y + obj.height / 2)
+
+                # vertical
+                else:
+
+                    direcao_movi = "y"
+                    pos_inicial = (obj.x + obj.width / 2, obj.y)
+                    pos_final = (obj.x + obj.width / 2, obj.y + obj.height)
+                vel = obj.properties["Vel"]
+                Plataforma_Movel((self.all_sprites, self.semi_collision_sprites), pos_inicial, pos_final, direcao_movi, vel)
+
+        # for obj in tmx_map.get_layer_by_name("Inimigos"):
+        #    if obj.name == "Enemy":
+        #        player((obj.x, obj.y), self.all_sprites, self.enemy_collision_sprites)
+
+    def map_change(self, num_nivel):
+        if self.jogador.hitbox_rect.colliderect(self.level_finish_rect) and self.jogador2.hitbox_rect.colliderect(
+                self.level_finish_rect):
+            #  num_nivel = 1 + num_nivel
+
+            #  print(num_nivel)
+            self.switch_stage(num_nivel)
+
+    def press_botao(self):
+        if self.jogador.hitbox_rect.colliderect(self.botao) or self.jogador2.hitbox_rect.colliderect(self.botao):
+            self.pressed_botao = True
+
+    def dano(self):
+        for sprite in self.dano_sprites:
+            if sprite.rect.colliderect(self.jogador2.hitbox_rect) or sprite.rect.colliderect(self.jogador.hitbox_rect):
+                #self.hit = True
+                self.jogador2.respawn()
+                self.jogador.respawn()
+
+    def run(self, dt):
+        self.display_surface.fill("#72647d")
+
+        self.all_sprites.update(dt)
+
+        x = ((self.jogador.hitbox_rect.x + self.jogador2.hitbox_rect.x) / 2)
+        y = ((self.jogador.hitbox_rect.y + self.jogador2.hitbox_rect.y) / 2)
+        center = [x, y]
+        self.all_sprites.draw(center)
+        self.map_change(self.num_niveis)
+        self.press_botao()
+        self.dano()
